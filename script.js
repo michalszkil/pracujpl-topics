3866c2fb96ab0337587c79d8a0484f64db95b442
Michał Szkil <michalszkil@gmail.com>
2022-02-25 15:43:42 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const select = document.getElementById("categories_select")
+
+select.addEventListener('change', function () {
+    let target = this.value;
+    // console.log(target)
+    document.getElementById("text-content").data="LDA_WEB\\" + target + ".txt";
+    document.getElementById("iframe-lda").src="LDA_WEB\\" + target + "_LDA.html";
+    document.getElementById("wordcloud").src="LDA_WEB\\" + target + "_wordcloud.png";
+    document.getElementById("wordcloud_bigrams").src="LDA_WEB\\" + target + "_wordcloud_bigrams.png";
+    document.getElementById("wordcloud_trigrams").src="LDA_WEB\\" + target + "_wordcloud_trigrams.png";
+    document.getElementById("barplot").src="LDA_WEB\\" + target + "_barplot.png";
+
+});
