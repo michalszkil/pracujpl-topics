2e83c791dab802cf66e320c8a085d985ab6638ca
Michał Szkil <michalszkil@gmail.com>
2022-03-23 10:20:44 +0100

    Added customized histograms

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ select.addEventListener('change', function () {
     document.getElementById("iframe-regression").src="LDA_WEB\\" + target + "_regression.html";
     document.getElementById("dominant-iframe-regression").src="LDA_WEB\\" + target + "_dominant_topic_regression.html";
     document.getElementById("iframe-quantile-regression").src="LDA_WEB\\" + target + "_quantile.html";
-    document.getElementById("histogram-plot").src="LDA_histograms\\" + target + "_histogram.html";
+    document.getElementById("histogram-plot").src="LDA_histograms\\" + target + "_histogram_custom_bins.html";
     tsne_select.value = "30";
     tsne_select_extended.value = "";
 });
@@ -23,7 +23,15 @@ const tsne_select = document.getElementById("categories-tsne-plot-select")
 tsne_select.addEventListener('change', function () {
     let target = this.value;
     document.getElementById("tsne-plot").src = "LDA_WEB\\" + select.value + "_" + target + "_tsne_plot" + tsne_select_extended.value +".html";
-    console.log("LDA_WEB\\" + select.value + "_" + target + "_tsne_plot.html");
+    // console.log("LDA_WEB\\" + select.value + "_" + target + "_tsne_plot.html");
+})
+
+const histogram_bins_select = document.getElementById("histogram-bins-plot-select")
+
+histogram_bins_select.addEventListener('change', function () {
+    let target = this.value;
+    document.getElementById("histogram-plot").src = "LDA_histograms\\" + select.value + "_histogram" + target + ".html";
+    // console.log("LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html");
 })
 
 const tsne_select_extended = document.getElementById("categories-tsne-plot-select-extended")
@@ -31,8 +39,9 @@ const tsne_select_extended = document.getElementById("categories-tsne-plot-selec
 tsne_select_extended.addEventListener('change', function () {
     let target = this.value;
     document.getElementById("tsne-plot").src = "LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html";
-    console.log("LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html");
+    // console.log("LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html");
 })
+
 
 
 let _query = document.getElementById('text-content').innerHTML
