c920c46708b3ef50238fc2b79c9cc176ea31e611
Michał Szkil <michalszkil@gmail.com>
2022-03-24 08:37:30 +0100

    Updated existing T-SNE plots, added SVD topics based plots

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ select.addEventListener('change', function () {
     // console.log(target)
     document.getElementById("text-content").data="LDA_WEB\\" + target + ".txt";
     document.getElementById("iframe-lda").src="LDA_WEB\\" + target + "_LDA.html";
-    document.getElementById("wordcloud").src="LDA_WEB\\" + target + "_wordcloud.png";
-    document.getElementById("wordcloud_bigrams").src="LDA_WEB\\" + target + "_wordcloud_bigrams.png";
-    document.getElementById("wordcloud_trigrams").src="LDA_WEB\\" + target + "_wordcloud_trigrams.png";
+    document.getElementById("wordcloud").src="wordclouds\\" + target + "_wordcloud.png";
+    document.getElementById("wordcloud_bigrams").src="wordclouds\\" + target + "_wordcloud_bigrams.png";
+    document.getElementById("wordcloud_trigrams").src="wordclouds\\" + target + "_wordcloud_trigrams.png";
     document.getElementById("barplot").src="LDA_WEB\\" + target + "_barplot.png";
     document.getElementById("tsne-plot").src="LDA_WEB\\" + target + "_30_tsne_plot.html";
+    document.getElementById("tsne-svd-plot").src="LDA_WEB\\" + target + "_30_tsne_svd_plot.html";
     document.getElementById("iframe-regression").src="LDA_WEB\\" + target + "_regression.html";
     document.getElementById("dominant-iframe-regression").src="LDA_WEB\\" + target + "_dominant_topic_regression.html";
     document.getElementById("iframe-quantile-regression").src="LDA_WEB\\" + target + "_quantile.html";
     document.getElementById("histogram-plot").src="LDA_histograms\\" + target + "_histogram_custom_bins.html";
     tsne_select.value = "30";
     tsne_select_extended.value = "";
+    tsne_svd_select.value = "30";
+    tsne_svd_select_extended.value = "";
 });
 
 const tsne_select = document.getElementById("categories-tsne-plot-select")
@@ -25,6 +28,15 @@ tsne_select.addEventListener('change', function () {
     document.getElementById("tsne-plot").src = "LDA_WEB\\" + select.value + "_" + target + "_tsne_plot" + tsne_select_extended.value +".html";
     // console.log("LDA_WEB\\" + select.value + "_" + target + "_tsne_plot.html");
 })
+
+const tsne_svd_select = document.getElementById("categories-tsne-svd-plot-select")
+
+tsne_svd_select.addEventListener('change', function () {
+    let target = this.value;
+    document.getElementById("tsne-svd-plot").src = "LDA_WEB\\" + select.value + "_" + target + "_tsne_svd_plot" + tsne_select_extended.value +".html";
+    // console.log("LDA_WEB\\" + select.value + "_" + target + "_tsne_plot.html");
+})
+
 
 const histogram_bins_select = document.getElementById("histogram-bins-plot-select")
 
@@ -39,6 +51,14 @@ const tsne_select_extended = document.getElementById("categories-tsne-plot-selec
 tsne_select_extended.addEventListener('change', function () {
     let target = this.value;
     document.getElementById("tsne-plot").src = "LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html";
+    // console.log("LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html");
+})
+
+const tsne_svd_select_extended = document.getElementById("categories-tsne-svd-plot-select-extended")
+
+tsne_svd_select_extended.addEventListener('change', function () {
+    let target = this.value;
+    document.getElementById("tsne-svd-plot").src = "LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_svd_plot" + target + ".html";
     // console.log("LDA_WEB\\" + select.value + "_" + tsne_select.value + "_tsne_plot" + target + ".html");
 })
 
